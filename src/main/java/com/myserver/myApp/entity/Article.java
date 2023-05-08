ee08968fceb14590f3e2cf87ece54e09a4aa4e1a
junman <junman_choi@tmax.co.kr>
2023-05-08 18:54:34 +0900

    fix:더미데이터에 의한 키값 중복 해결

--- src/main/java/com/myserver/myApp/entity/Article.java
+++ src/main/java/com/myserver/myApp/entity/Article.java
@@ -3,6 +3,7 @@ package com.myserver.myApp.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,11 +17,11 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor // 디폴트 생성자 추가
 @ToString
+@Getter
 public class Article {
 
     @Id
-    @GeneratedValue
-    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 id 자동생성
     private Long id;
 
     @Column
