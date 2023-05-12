24a78e701ba7eb871330b539dd088944ffbc834c
junman <junman_choi@tmax.co.kr>
2023-05-12 17:00:42 +0900

    feat: jwt 토큰 DTO 추가

---
+++ src/main/java/com/myserver/myApp/dto/auth/TokenDTO.java
@@ -0,0 +1,19 @@
+package com.myserver.myApp.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDTO {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private Long accessTokenExpiresIn;
+}
